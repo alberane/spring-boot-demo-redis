94fe688436263e93147cfb177d8e576cb4500e6e
alberane <alberanelucio@gmail.com>
2025-07-29 17:41:09 -0300

    Init

---
+++ src/test/java/com/example/spring_boot_demo_redis/SpringBootDemoRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_demo_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
